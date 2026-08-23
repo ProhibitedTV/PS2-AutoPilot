57c3e105db5c5164712ab1e59eec37025c6c7ae4
RhythmicCarnage <71204385+ProhibitedTV@users.noreply.github.com>
2026-08-22 18:36:40 -0600

    Promote Madden v0.5 profile

--- src/ps2_autopilot/profiles/__init__.py
+++ src/ps2_autopilot/profiles/__init__.py
@@ -1,6 +1,12 @@
 from .generic_chaos import GenericChaosProfile
 from .madden2005_v4 import Madden2005V4Profile
+from .madden2005_v5 import Madden2005V5Profile
 
-Madden2005Profile = Madden2005V4Profile
+Madden2005Profile = Madden2005V5Profile
 
-__all__ = ["GenericChaosProfile", "Madden2005Profile", "Madden2005V4Profile"]
+__all__ = [
+    "GenericChaosProfile",
+    "Madden2005Profile",
+    "Madden2005V4Profile",
+    "Madden2005V5Profile",
+]
