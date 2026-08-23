31418fb67ceee3ccc08de55ce2c991ac53adb78f
RhythmicCarnage <71204385+ProhibitedTV@users.noreply.github.com>
2026-08-23 11:43:32 -0600

    Expose Jak V2 as current profile alias

--- src/ps2_autopilot/profiles/__init__.py
+++ src/ps2_autopilot/profiles/__init__.py
@@ -1,5 +1,6 @@
 from .generic_chaos import GenericChaosProfile
-from .jak_and_daxter import JakAndDaxterProfile, JakPhase
+from .jak_and_daxter import JakAndDaxterProfile as JakAndDaxterV1Profile, JakPhase
+from .jak_and_daxter_v2 import JakAndDaxterV2Profile
 from .madden2005_v4 import Madden2005V4Profile
 from .madden2005_v5 import Madden2005V5Profile
 from .madden2005_v6 import Madden2005V6Profile
@@ -21,6 +22,7 @@ from .madden2005_v20 import Madden2005V20Profile
 from .madden2005_v21 import Madden2005V21Profile
 from .madden2005_v22 import Madden2005V22Profile
 
+JakAndDaxterProfile = JakAndDaxterV2Profile
 Madden2005Profile = Madden2005V22Profile
 
 from .registry import (  # noqa: E402
@@ -34,6 +36,8 @@ from .registry import (  # noqa: E402
 __all__ = [
     "GenericChaosProfile",
     "JakAndDaxterProfile",
+    "JakAndDaxterV1Profile",
+    "JakAndDaxterV2Profile",
     "JakPhase",
     "Madden2005Profile",
     "Madden2005V4Profile",
