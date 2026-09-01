6806b85ce738dd8d6680d76f17c14cf1005185c1
RhythmicCarnage <71204385+ProhibitedTV@users.noreply.github.com>
2026-09-01 13:51:10 -0600

    Export NFS HP2 V9 profile

--- src/ps2_autopilot/profiles/__init__.py
+++ src/ps2_autopilot/profiles/__init__.py
@@ -59,10 +59,11 @@ from .nfs_hot_pursuit_2_v5 import NfsHotPursuit2V5Profile
 from .nfs_hot_pursuit_2_v6 import NfsHotPursuit2V6Profile
 from .nfs_hot_pursuit_2_v7 import NfsHotPursuit2V7Profile
 from .nfs_hot_pursuit_2_v8 import NfsHotPursuit2V8Profile
+from .nfs_hot_pursuit_2_v9 import NfsHotPursuit2V9Profile
 
 JakAndDaxterProfile = JakAndDaxterV22Profile
 Madden2005Profile = Madden2005V32Profile
-NfsHotPursuit2Profile = NfsHotPursuit2V8Profile
+NfsHotPursuit2Profile = NfsHotPursuit2V9Profile
 
 from .registry import (  # noqa: E402
     ProfileSpec,
@@ -138,6 +139,7 @@ __all__ = [
     "NfsHotPursuit2V6Profile",
     "NfsHotPursuit2V7Profile",
     "NfsHotPursuit2V8Profile",
+    "NfsHotPursuit2V9Profile",
     "NfsPhase",
     "NfsRoute",
     "NfsScreen",
